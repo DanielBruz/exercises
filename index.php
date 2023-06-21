8436cbd0d2c095b8dc5e7ea5a94b335c89d5ea45
DanielBruz <daniel.bruzenak@tiscali.cz>
2023-06-21 13:16:31 +0200

    Presun hlavicky do header.php

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<title>fun times php</title>
-
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/main.css">
-</head>
-<body class="container">
-
-	<div class="page-header">
-		<h1 class="text-muted">fun times php</h1>
-
-	</div>
 		<?php
 			// ulozime hodnoty do promennych
 			$item_count = 5;
@@ -97,5 +79,3 @@
 
 			echo '<br>----<br>';
 		?>
-</body>
-</html>
